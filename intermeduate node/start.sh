039738a59cc01822ecec7c6d5b408ff5cbc20be1
shovaltesting <75573478+shovaltesting@users.noreply.github.com>
2021-10-21 16:24:12 +0300

    Add files via upload

---
+++ intermeduate node/start.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+if [ -z "$REDIRECT_TYPE" ]; then
+	REDIRECT_TYPE="permanent"
+fi
+
+if [ -z "$REDIRECT_TARGET" ]; then
+	echo "Redirect target variable not set (REDIRECT_TARGET)"
+	exit 1
+else
+	# Add http if not set
+	if ! [[ $REDIRECT_TARGET =~ ^https?:// ]]; then
+		REDIRECT_TARGET="http://$REDIRECT_TARGET"
+	fi
+
+	# Add trailing slash
+	if [[ ${REDIRECT_TARGET:length-1:1} != "/" ]]; then
+		REDIRECT_TARGET="$REDIRECT_TARGET/"
+	fi
+fi
+
+# Default to 443
+LISTEN="443"
+# Listen to PORT variable given on Cloud Run Context
+if [ ! -z "$PORT" ]; then
+	LISTEN="$PORT"
+fi
+
+cat <<EOF > /etc/nginx/conf.d/default.conf
+server {
+	listen ${LISTEN};
+
+	rewrite ^/(.*)\$ ${REDIRECT_TARGET}\$1 ${REDIRECT_TYPE};
+}
+EOF
+
+
+echo "Listening to $LISTEN, Redirecting HTTP requests to ${REDIRECT_TARGET}..."
+
+exec nginx -g "daemon off;"
